f0d5ad08d7ff038ba1535e04022e507e4f64e41d
manoj.mahato@digihub.com.np <Manoj@554Kumar>
2023-11-06 09:33:43 +0545

    Reboot Scheduled

--- ROOT/src/main/java/com/root/service/WelcomeController.java
+++ ROOT/src/main/java/com/root/service/WelcomeController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class WelcomeController {
+    @GetMapping()
+    public String get() {
+        return "Welcome DIT Solution";
+    }
 
     @GetMapping(".well-known/pki-validation/F8F862EC218B1C8E3EDFDB4D2584A935.txt")
-    public String get() {
+    public String ssl() {
         return "29D6EA42696D5168C4819BE7943621C62964D1FB3B8831D727ABDAA92AB80454\n" +
                 "comodoca.com\n" +
                 "ff0fcbb5051a6ac";
