812d377ca4ca02fdaa8059d36f3968379953aa49
Manoj <msware9@gmail.com>
2024-04-09 07:12:52 +0545

    Reboot Scheduled

--- ROOT/src/main/java/com/root/service/MsController.java
+++ ROOT/src/main/java/com/root/service/MsController.java
@@ -1,17 +1,12 @@
 package com.root.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Objects;
 
 @RestController
 @Slf4j
