4073caee663933cc426de265f14169bd714dae9d
manoj <msware9@gmail.com>
2025-07-01 08:51:25 +0545

    BMB

--- SchoolDatabaseConf/src/main/java/com/model/exam/ExamSchedule.java
+++ SchoolDatabaseConf/src/main/java/com/model/exam/ExamSchedule.java
@@ -16,19 +16,19 @@ public class ExamSchedule {
     @Id
     @Column(name = "id")
     private long id;
-    @Column(name = "exam_id")
+    @Column(name = "exam_id",nullable = false)
     private long examId;
-    @Column(name = "program_id")
+    @Column(name = "program_id",nullable = false)
     private long programId;
-    @Column(name = "class_id")
+    @Column(name = "class_id",nullable = false)
     private long classId;
-    @Column(name = "exam_date")
+    @Column(name = "exam_date",nullable = false)
     @Temporal(TemporalType.DATE)
     private Date examDate;
-    @Column(name = "start_time")
+    @Column(name = "start_time",nullable = false)
     @Temporal(TemporalType.TIME)
     private Date startTime;
-    @Column(name = "end_time")
+    @Column(name = "end_time",nullable = false)
     @Temporal(TemporalType.TIME)
     private Date endTime;
 
